3e781ece40b9c838698416a15054546d30de866f
Dhanraj Pimple <dhanrajpimple16012001@gmail.com>
2026-04-27 10:47:20 +0530

    deploy

--- backend/src/middlelayer/routes/admin.routes.ts
+++ backend/src/middlelayer/routes/admin.routes.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express';
 import { CompanyController } from '../controllers/company.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
-import { UserController } from '../controllers/user.controller';
-import { OrgController } from '../controllers/org.controller';
-import { RoleController } from '../controllers/role.controller';
+
 
 /**
  * ADMIN ROUTES LOGIC:
@@ -16,20 +14,6 @@ router.use(authMiddleware);
 router.post('/groups', CompanyController.getGroupCompanies);
 // ----------------------------------------------------------
 
-// -------------user routes----------------------------------
-router.post('/user/fetch-all', UserController.fetchAllUsers);
-router.post('/user/initiate', UserController.initiateUserOnboarding);
-router.post('/user/action', UserController.actionUserOnboarding);
-// ----------------------------------------------------------
 
-//--------------org routes-----------------------------------
-router.post('/org/initiate', OrgController.initiateOrgRequest);
-router.post('/org/approve', OrgController.approveOrgRequest);
-router.post('/org/fetch', OrgController.fetchOrgStructure);
-// ----------------------------------------------------------
-
-// --------------roles routes--------------------------------
-router.post('/role/fetch-all', RoleController.fetchAllRoles);
-// ----------------------------------------------------------
 
 export default router;
